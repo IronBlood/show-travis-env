84ff8f361c1553af5a70df1081a056979af33331
IronBlood <aplshenyang@gmail.com>
2015-04-05 12:46:55 +0800

    将分支的编译任务放到 before_script 中

--- run.sh
+++ run.sh
@@ -5,12 +5,3 @@ uname -a
 gcc --version
 file `which file`
 
-git clone https://github.com/bmybbs/bmybbs.git
-cd bmybbs
-git checkout feature/bmyapp
-sudo mkdir -p /home/bbs/bin
-cd ythtlib
-sudo make install
-cd ../libythtbbs
-sudo make install
-
